27c13dbc7c11dbe8dd72c1308fe8fb0ac450bb96
Ansari Zahoor <ansarizahoor7@gmail.com>
2022-05-08 18:42:03 +0530

    Updates

---
+++ precache-manifest.42eb0d1ffe342a73a79ae7015b1b12b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5bab693cbe958a70481",
+    "url": "/Textutils-App/static/css/main.f04b8fe7.chunk.css"
+  },
+  {
+    "revision": "f5bab693cbe958a70481",
+    "url": "/Textutils-App/static/js/main.f5bab693.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/Textutils-App/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "9f467190b4df4fe2318e",
+    "url": "/Textutils-App/static/js/2.9f467190.chunk.js"
+  },
+  {
+    "revision": "e369377b822f8abc3430",
+    "url": "/Textutils-App/static/js/runtime~main.e369377b.js"
+  },
+  {
+    "revision": "f5e5fab1d7830191743e4b9e30747022",
+    "url": "/Textutils-App/index.html"
+  }
+];
